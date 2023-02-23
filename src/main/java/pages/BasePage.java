d43fe6e7f81acd96d149a36a273a012f318abbf4
olgagantvorg <40515031+olgagantvorg@users.noreply.github.com>
2023-02-23 16:40:48 +0200

    1

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,45 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait webDriverWait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        webDriverWait = new WebDriverWait(driver,30);
+    }
+    public void typeText(WebElement element, String text){
+        webDriverWait.until(ExpectedConditions.visibilityOf(element));
+        element.click();
+        element.clear();
+        element.sendKeys(text);
+    }
+    public void clickElement (WebElement element){
+        System.out.println("Trying to click element " + element);
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+        System.out.println("Successfull click to element " + element);
+
+    }
+    public String getElementText(WebElement element){
+        return element.getText();
+    }
+    public void sleep(int timeout){
+        try {
+            Thread.sleep(timeout);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- src/test/java/successTest/SuccessTests.java
+++ src/test/java/successTest/SuccessTests.java
@@ -1,12 +1,34 @@
 package successTest;
 
-import org.testng.Assert;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import pages.LoginPage;
 
 public class SuccessTests {
+    protected WebDriver driver;
     @Test
 
     public void testStandardUser(){
-        Assert.assertEquals(6,6);
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user","secret_sauce");
+
+        driver.quit();
+    }
+    @Test
+    public void testProblemUser(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("problem_user","secret_sauce");
+
+        driver.quit();
     }
 }
